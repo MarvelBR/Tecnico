137c566d8b50c7bb2138474850db47843e4a2023
Erick Molina Gehring <113470770+MarvelBR@users.noreply.github.com>
2025-06-15 21:08:57 -0300

    Update DW

---
+++ DW/exemplo 2 java/script.js
@@ -0,0 +1,33 @@
+
+
+let nome = "Rafael"
+
+let x  = 5
+let y = x * 3
+
+
+// ENTRADA DE DADOS
+// nome = prompt("Digite seu nome")
+console.log(nome)
+console.log(y)
+
+// DESVIO CONDICIONAL
+if (y % 2 == 0){
+    console.log("PAR")
+}else{
+    console.log("ÍMPAR")
+}
+
+// REPETIÇÕES
+for(let i = 0; i<10; i++){
+    console.log(i)
+}
+
+// LISTA - VETOR
+let lista = [10,20,30]
+console.log(lista)
+console.log(lista[1])
+lista.push(40)
+console.log(lista)
+
+
